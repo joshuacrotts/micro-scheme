296db3fb221a41f544283f88659db3aee8c500df
Joshua Crotts <joshuacrotts@yahoo.com>
2022-01-22 13:20:31 -0500

    Added a few more comments.

--- src/main/java/com/joshuacrotts/minischeme/ast/MSLambdaNode.java
+++ src/main/java/com/joshuacrotts/minischeme/ast/MSLambdaNode.java
@@ -83,24 +83,31 @@ public class MSLambdaNode extends MSSyntaxTree {
         return this.isClosure;
     }
 
+    /**
+     *
+     */
     private void determineIfClosure() {
         Queue<Pair<MSSyntaxTree, MSLambdaNode>> queue = new LinkedList<>();
         Pair<MSSyntaxTree, MSLambdaNode> root = new Pair<>(this, this);
         queue.add(root);
         while (!queue.isEmpty()) {
             Pair<MSSyntaxTree, MSLambdaNode> curr = queue.poll();
+            // If it's a lambda, then we want to add all of its children and
+            // tell its pair that a is the lambda we want to check.
             if (curr.a.isLambda()) {
                 for (MSSyntaxTree ch : curr.a.getChildren()) {
                     queue.add(new Pair<>(ch, (MSLambdaNode) curr.a));
                 }
                 continue;
             } else if (curr.a.isVariable()) {
+                // Otherwise, if it's a variable, check it against its lambda parent.
                 if (this.closureConditions((MSVariableNode) curr.a, curr.b)) {
                     this.isClosure = true;
                     return;
                 }
             }
 
+            // If it's not a variable or it was found as a match, just add all the children.
             for (MSSyntaxTree ch : curr.a.getChildren()) {
                 queue.add(new Pair<>(ch, curr.b));
             }
@@ -109,6 +116,12 @@ public class MSLambdaNode extends MSSyntaxTree {
         this.isClosure = false;
     }
 
+    /**
+     *
+     * @param var
+     * @param lambdaNode
+     * @return
+     */
     private boolean closureConditions(MSVariableNode var, MSLambdaNode lambdaNode) {
         boolean matchesParam = this.matchesParameter(var, lambdaNode);
         boolean matchesEnvSymbol = this.matchesEnvironmentSymbol(var);
@@ -116,6 +129,12 @@ public class MSLambdaNode extends MSSyntaxTree {
         return !matchesParam && !matchesEnvSymbol && !isBuiltin;
     }
 
+    /**
+     *
+     * @param var
+     * @param lambdaRoot
+     * @return
+     */
     private boolean matchesParameter(MSVariableNode var, MSLambdaNode lambdaRoot) {
         ArrayList<MSSyntaxTree> lambdaParameters = lambdaRoot.getLambdaParameters();
         for (int i = 0; i < lambdaRoot.NUM_LAMBDA_PARAMETERS; i++) {
@@ -128,6 +147,11 @@ public class MSLambdaNode extends MSSyntaxTree {
         return false;
     }
 
+    /**
+     *
+     * @param var
+     * @return
+     */
     private boolean matchesEnvironmentSymbol(MSVariableNode var) {
         for (int i = 0; i < this.closureEnvironment.numberOfBindings(); i++) {
             MSSyntaxTree envVar = this.closureEnvironment.findInEnvironment(var);
